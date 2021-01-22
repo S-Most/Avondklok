1e5a2aaa886c3cc5fc7e1d297524bc8d1bd46557
HoornbloemCoding <71282824+HoornbloemCoding@users.noreply.github.com>
2021-01-22 21:37:09 +0100

    dynamic Avondklok and styling

--- js/timer.js
+++ js/timer.js
@@ -1,37 +1,55 @@
-const objDate = new Date();
-let hours = objDate.getHours();
-let minuts = objDate.getMinutes();
-let timer = "leeg";
-let pushHTML;
+const vrijheidsEle = document.getElementById("vrijheid");
+const avondklokEle = document.getElementById("avondklok");
 
 function prettyDate2(timeleft) {
     let array = [];
     let time = timeleft % (60 * 60 * 1000 * 24);
-    let hours = time / (60 * 60 * 1000);
-    let minuts = (time / (60 * 1000)) % 60;
+    let hours = (time / (60 * 60 * 1000) - 0, 5);
+    let minuts = (((time / (60 * 1000)) % 60) - 0, 5);
     let seconds = (time / 1000) % 60;
     array.push(hours.toFixed(0), minuts.toFixed(0), seconds.toFixed(0));
     console.log(array);
     return array;
 }
 
+const showAvondklok = () => {
+    avondklokEle.style.display = "flex";
+    vrijheidsEle.style.display = "none";
+};
+
+const showVrijheid = () => {
+    vrijheidsEle.style.display = "flex";
+    avondklok.style.display = "none";
+};
+
 const checkTime = () => {
-    checkAvondklok();
+    if (checkAvondklok()) {
+        showAvondklok();
+    } else {
+        minutesLeft();
+        showVrijheid();
+    }
+};
+
+const minutesLeft = () => {
     curTime = new Date().getTime();
     _21Time = new Date(2022, 0, 1, 21, 0, 0, 0).getTime();
     let showTime = _21Time - curTime;
-    console.log("time left in miniseconds?", showTime);
-    pushHTML = prettyDate2(showTime);
+    let pushHTML = prettyDate2(showTime);
     document.getElementById("tijd").innerText =
         pushHTML[0] +
         "uur, " +
         pushHTML[1] +
-        "minuten, " +
+        "minuten, en " +
         pushHTML[2] +
         "seconden";
 };
 
 const checkAvondklok = () => {
+    const objDate = new Date();
+    let hours = objDate.getHours();
+    let minutes = objDate.getMinutes();
+
     if (
         (hours >= 21 && hours <= 24) ||
         (hours >= 0 && hours < 4) ||
@@ -44,5 +62,5 @@ const checkAvondklok = () => {
         return false;
     }
 };
-
-setInterval(checkTime(), 1000);
+let setInterval = (checkTime(), 1000);
+checkTime();
